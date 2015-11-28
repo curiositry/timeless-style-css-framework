898191c3d303ed636ff0f60d54ff179d0a39c4f8
Curiositry <isa@automathic.org>
2015-11-27 18:06:36 -0700

    Add starting pullquote js (needs cleanup)

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// Pullquotes
+document.addEventListener("DOMContentLoaded", function(event) {
+  function pullQuote(el){
+    var parentParagraph = el.parentNode;
+    parentParagraph.style.position = 'relative';
+    var clone = el.cloneNode(true);
+    clone.setAttribute('class','semantic-pull-quote--pulled');
+    // Hey y’all, watch this!
+    parentParagraph.parentNode.parentNode.insertBefore(clone, parentParagraph.parentNode);
+    
+    if(!clone.getAttribute('data-content')){
+      clone.setAttribute('data-content', clone.innerHTML );
+      clone.innerHTML = null;
+    }
+  };
+  
+  var pullQuotes = document.getElementsByClassName('semantic-pull-quote');
+  
+  for(var i = 0; i < pullQuotes.length; i++) {
+    pullQuote(pullQuotes[i]);
+  }
+
+  computeBodyPadding();
+});
+
+window.onresize = function(event) {
+  computeBodyPadding();
+};
